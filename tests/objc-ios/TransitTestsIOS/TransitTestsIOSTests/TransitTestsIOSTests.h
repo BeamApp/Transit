ce9616001004b5f9cbe9890d22839979fbe3dc96
Heiko Behrens <HeikoBehrens@gmx.de>
2013-02-08 15:56:11 +0100

    naked iOS app with failing unit test

---
+++ tests/objc-ios/TransitTestsIOS/TransitTestsIOSTests/TransitTestsIOSTests.h
@@ -0,0 +1,13 @@
+//
+//  TransitTestsIOSTests.h
+//  TransitTestsIOSTests
+//
+//  Created by Heiko Behrens on 08.02.13.
+//  Copyright (c) 2013 BeamApp. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TransitTestsIOSTests : SenTestCase
+
+@end
