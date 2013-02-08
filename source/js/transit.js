ec4bd76e77e94369ef4cbca1729e51afd5beaf29
Heiko Behrens <HeikoBehrens@gmx.de>
2013-02-08 10:24:28 +0100

    setup for TDD

---
+++ source/js/transit.js
@@ -0,0 +1,30 @@
+(function(){
+    var transit = {};
+
+    transit.requestCall = function(callRequest){
+        throw "must be replaced by native runtime";
+    };
+
+    transit.nativeFunction = function(nativeId){
+        var f = function(){
+            transit.performCall(nativeId, this, arguments);
+        };
+        f.transitNativeId = nativeId;
+        return f
+    };
+
+    transit.performCall = function(nativeId, thisArg, otherArgs) {
+        throw "to be implemented"
+    };
+
+    transit.retainProxy = function(element){
+        throw "to be implemented";
+    };
+
+    transit.releaseProxy = function(proxyId) {
+        throw "to be implemented";
+    };
+
+    window.transit = transit;
+
+})();
---
+++ source/tests/spec/transitSpec.js
@@ -0,0 +1,7 @@
+/*global jasmine, describe, it, xit, expect, beforeEach, afterEach, runs, waitsFor */
+
+describe("Transit", function() {
+    it("exists", function(){
+        expect(transit).toBeDefined();
+    })
+});
