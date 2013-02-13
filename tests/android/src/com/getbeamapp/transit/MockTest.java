ddb0133b9eded8b302ba654b53c0fc068d9fcd80
Marcel Jackwerth <marceljackwerth@gmail.com>
2013-02-13 14:45:12 +0100

    test AndroidMock.verify

--- tests/android/src/com/getbeamapp/transit/MockTest.java
+++ tests/android/src/com/getbeamapp/transit/MockTest.java
@@ -37,8 +37,19 @@ public class MockTest extends TestCase {
         AbstractTransitContext ctx = new TransitContext(noopAdapter);
         TransitProxy proxy = AndroidMock.createMock(TransitProxy.class, ctx);
         AndroidMock.expect(proxy.get()).andReturn(42);
+        AndroidMock.expect(proxy.get()).andReturn(42);
         AndroidMock.replay(proxy);
         assertEquals(42, proxy.get());
-        AndroidMock.verify(proxy);
+
+        try {
+            AndroidMock.verify(proxy);
+            fail();
+        } catch (AssertionError e) {
+            // WORKAROUND: EasyMock creates oddly formatted messages with
+            // newlines and tabs - let's get rid of multiple whitespace chars.
+            String msg = e.getMessage().trim().replaceAll("[\\r\\n\\s\\t]+", " ");
+            assertEquals("Expectation failure on verify: get(): expected: 2, actual: 1", msg);
+        }
+
     }
 }
