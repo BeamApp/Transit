b0219503ee2545dbb0d2eed21d5fc362e275cde5
Marcel Jackwerth <marceljackwerth@gmail.com>
2013-02-21 18:44:45 +0100

    junit report xml

--- scripts/build-java-android.sh
+++ scripts/build-java-android.sh
@@ -102,7 +102,7 @@ run_tests()
   adb wait-for-device
   ant emma debug
 # deactivate tests for now
-#  ant emma installt test
+#  ant emma installt test fetch-report
   popd
 
   echo "Building test app and run tests [DONE]"
