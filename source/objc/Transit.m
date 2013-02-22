e48ced219b240510c36344d2d563c0d3f60c3297
Heiko Behrens <HeikoBehrens@gmx.de>
2013-02-22 16:25:19 +0100

    objectForKey and friends objc TransitObject + simplified examples accordingly

--- source/objc/Transit.m
+++ source/objc/Transit.m
@@ -147,12 +147,48 @@ NSError* errorWithCodeFromException(NSUInteger code, NSException* exception) {
     _context = nil;
 }
 
-- (id)objectForKey:(NSString *)string {
-    @throw [NSException exceptionWithName:@"TransitException" reason:@"not supported" userInfo:nil];
+- (id)objectForKey:(id)key{
+    return [self.context eval:@"@[@]" val:self val:key];
 }
 
-- (BOOL)callMember:(NSString *)string arguments:(NSArray *)arguments {
-    @throw [NSException exceptionWithName:@"TransitException" reason:@"not supported" userInfo:nil];
+- (void)setObject:(id)object forKey:(id < NSCopying >)key {
+    [self.context eval:@"@[@]=@" val:self val:key val:object];
+}
+
+- (id)objectAtIndexedSubscript:(NSUInteger)idx {
+    return [self objectForKey:@(idx)];
+}
+
+- (void)setObject:(id)obj atIndexedSubscript:(NSInteger)idx {
+    [self setObject:obj forKey:@(idx)];
+}
+
+- (id)objectForKeyedSubscript:(id)key {
+    return [self objectForKey:key];
+}
+
+- (void)setObject:(id)obj forKeyedSubscript:(id <NSCopying>)key {
+    [self setObject:obj forKey:key];
+}
+
+- (id)callMember:(NSString *)string {
+    return [self callMember:string arguments:@[]];
+}
+
+- (id)callMember:(NSString *)string arg:(id)arg0 {
+    return [self callMember:string arguments:@[arg0]];
+}
+
+- (id)callMember:(NSString *)string arg:(id)arg0 arg:(id)arg1 {
+    return [self callMember:string arguments:@[arg0, arg1]];
+}
+
+- (id)callMember:(NSString *)string arg:(id)arg0 arg:(id)arg1 arg:(id)arg2 {
+    return [self callMember:string arguments:@[arg0, arg1, arg2]];
+}
+
+- (id)callMember:(NSString *)string arguments:(NSArray *)arguments {
+    return [self.context eval:@"@[@].apply(@,@)" values:@[self, string, self, arguments]];
 }
 
 @end
--- tests/objc-ios/TransitTestsIOSTests/TransitObjectTests.m
+++ tests/objc-ios/TransitTestsIOSTests/TransitObjectTests.m
@@ -40,6 +40,66 @@
     STAssertNil(object.context, @"context removed");
 }
 
+-(void)testObjectForKey {
+    id context = [OCMockObject mockForClass:TransitContext.class];
+    TransitObject *object = [TransitObject.alloc initWithContext:context];
+
+    [[[context stub] andReturn:@"value"] eval:@"@[@]" val:object val:@"key"];
+    id value = [object objectForKey:@"key"];
+
+    STAssertEqualObjects(@"value", value, @"returned correct value");
+    STAssertNoThrow([context verify], @"verify mock");
+
+    [[context expect] eval:@"@[@]=@" val:object val:@"key" val:@"value2"];
+    [object setObject:@"value2" forKey:@"key"];
+    STAssertNoThrow([context verify], @"verify mock");
+}
+
+-(void)testIndexedSubscript {
+    id context = [OCMockObject mockForClass:TransitContext.class];
+    TransitObject *object = [TransitObject.alloc initWithContext:context];
+
+    [[[context stub] andReturn:@"value"] eval:@"@[@]" val:object val:@1];
+    id value = object[1];
+
+    STAssertEqualObjects(@"value", value, @"returned correct value");
+    STAssertNoThrow([context verify], @"verify mock");
+
+    [[context expect] eval:@"@[@]=@" val:object val:@2 val:@"another value"];
+    object[2] = @"another value";
+    STAssertNoThrow([context verify], @"verify mock");
+}
+
+-(void)testKeyedSubscript {
+    id context = [OCMockObject mockForClass:TransitContext.class];
+    id  object = [TransitObject.alloc initWithContext:context];
+
+    [[[context stub] andReturn:@"value"] eval:@"@[@]" val:object val:@"key"];
+    id value = object[@"key"];
+
+    STAssertEqualObjects(@"value", value, @"returned correct value");
+    STAssertNoThrow([context verify], @"verify mock");
+
+    [[context expect] eval:@"@[@]=@" val:object val:@"key" val:@"another value"];
+    object[@"key"] = @"another value";
+    STAssertNoThrow([context verify], @"verify mock");
+}
+
+-(void)testCallMember {
+    id context = [OCMockObject mockForClass:TransitContext.class];
+    id  object = [TransitObject.alloc initWithContext:context];
+
+    NSArray* arguments = @[@1,@2,@3];
+
+    [[[context stub] andReturn:@"value"] eval:@"@[@].apply(@,@)" values:@[object, @"someMethod", object, arguments]];
+    id value = [object callMember:@"someMethod" arguments:arguments];
+
+    STAssertEqualObjects(@"value", value, @"returned correct value");
+    STAssertNoThrow([context verify], @"verify mock");
+
+
+}
+
 
 
 @end
