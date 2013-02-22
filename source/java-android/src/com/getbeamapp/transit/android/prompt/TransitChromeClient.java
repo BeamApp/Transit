92ecb6a9bc339232c6f3507a368298d4548b3734
Marcel Jackwerth <marceljackwerth@gmail.com>
2013-02-22 17:25:43 +0100

    TransitChromeClient now logs JS console to LogCat with appropiate levels

--- source/java-android/src/com/getbeamapp/transit/android/prompt/TransitChromeClient.java
+++ source/java-android/src/com/getbeamapp/transit/android/prompt/TransitChromeClient.java
@@ -1,21 +1,25 @@
 package com.getbeamapp.transit.android.prompt;
 
+import android.util.Log;
+import android.webkit.ConsoleMessage;
 import android.webkit.JsPromptResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class TransitChromeClient extends WebChromeClient {
 
+    private static final String TAG = TransitChromeClient.class.getSimpleName();
+
     private TransitPromptAdapter transitAdapter;
 
     public TransitChromeClient() {
-        
+
     }
-    
+
     public TransitPromptAdapter getTransitAdapter() {
         return transitAdapter;
     }
-    
+
     public void setTransitAdapter(TransitPromptAdapter transitAdapter) {
         this.transitAdapter = transitAdapter;
     }
@@ -23,12 +27,33 @@ public class TransitChromeClient extends WebChromeClient {
     @Override
     public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
         TransitFuture<String> future = new TransitFuture<String>();
-        
+
         if (transitAdapter.onJSCall(message, defaultValue, future)) {
             result.confirm(future.block());
             return true;
         } else {
             return super.onJsPrompt(view, url, message, defaultValue, result);
         }
+    }
+
+    @Override
+    public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+        switch (consoleMessage.messageLevel()) {
+        case ERROR:
+            Log.e(TAG, consoleMessage.message());
+            break;
+        case WARNING:
+            Log.w(TAG, consoleMessage.message());
+            break;
+        case DEBUG:
+            Log.d(TAG, consoleMessage.message());
+            break;
+        case TIP:
+        case LOG:
+        default:
+            Log.i(TAG, consoleMessage.message());
+        }
+
+        return true;
     }
 }
