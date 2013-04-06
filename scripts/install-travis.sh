5ef53d6c430ab423708c592e18bab9fc6bad7603
Heiko Behrens <HeikoBehrens@gmx.de>
2013-04-06 19:54:52 +0200

    install missing android-sdk on travis env

--- scripts/install-travis.sh
+++ scripts/install-travis.sh
@@ -2,5 +2,6 @@
 set -e
 
 brew install phantomjs
+brew install android-sdk
 android update sdk -u --filter platform-tools,android-17,system-image,extra-intel-Hardware_Accelerated_Execution_Manager
 echo "no" | android create avd -n default -t android-17 -b x86
