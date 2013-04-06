86972d8d207f5ac651c94452f360358af7c57b63
Heiko Behrens <HeikoBehrens@gmx.de>
2013-04-06 17:48:57 +0200

    only install phantomjs for now

--- scripts/install_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-brew install phantomjs
-#brew install ios-sim
-#brew install android-sdk
-#android update sdk -u --filter platform-tools,android-17,system-image,extra-intel-Hardware_Accelerated_Execution_Manager
-#android create avd -n default -t android-17 -b x86
