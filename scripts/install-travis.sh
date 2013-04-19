c4ae2b7b5e488e2db2d6bbd73e810fbdf60b6b51
Marcel Jackwerth <marceljackwerth@gmail.com>
2013-04-19 11:05:11 +0300

    Custom homebrew installation

--- scripts/install-travis.sh
+++ scripts/install-travis.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+HOMEBREW=$HOME/homebrew
+mkdir $HOMEBREW
+curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOMEBREW
+export PATH=$HOMEBREW/bin:$PATH
+
 brew install phantomjs
 brew install android-sdk
 android update sdk -u --filter platform-tools,android-17,system-image,extra-intel-Hardware_Accelerated_Execution_Manager
