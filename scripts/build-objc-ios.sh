ef5c8b55b0a2aca3c246d2f8534859ddf1cc2bd8
Heiko Behrens <HeikoBehrens@gmx.de>
2013-06-04 18:02:49 +0200

    activate objc-tests, again (accidentally deactivated with 045bca8f0483b2f0ebb6c10a1c358350d4d9c023)

--- scripts/build-objc-ios.sh
+++ scripts/build-objc-ios.sh
@@ -17,10 +17,10 @@ TESTS_PROJECT_ROOT="`cd ../tests/objc-ios; pwd`"
 EXAMPLES_PROJECT_NAME="TransitExampleIOS"
 EXAMPLES_PROJECT_ROOT="`cd ../examples/objc-ios; pwd`"
 
-#xctool -workspace "$TESTS_PROJECT_ROOT/$TESTS_PROJECT_NAME.xcworkspace" -scheme $TESTS_PROJECT_NAME clean build test
+xctool -workspace "$TESTS_PROJECT_ROOT/$TESTS_PROJECT_NAME.xcworkspace" -scheme $TESTS_PROJECT_NAME build test
 
 # somehow, clean on example project seems to fail...
-#xctool -workspace "$EXAMPLES_PROJECT_ROOT/$EXAMPLES_PROJECT_NAME.xcworkspace" -scheme $EXAMPLES_PROJECT_NAME  build test
+xctool -workspace "$EXAMPLES_PROJECT_ROOT/$EXAMPLES_PROJECT_NAME.xcworkspace" -scheme $EXAMPLES_PROJECT_NAME  build test
 
 
 echo "Validate PodSpec"
