d21329eaa9e4dd015fab61449f5c6c9957703c14
Heiko Behrens <HeikoBehrens@gmx.de>
2013-07-02 10:47:19 +0200

    build script for OSX

---
+++ scripts/build-objc-osx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+
+TESTS_PROJECT_NAME="TransitTestsOSX"
+TESTS_PROJECT_ROOT="`cd ../tests/objc-osx; pwd`"
+
+echo "Build"
+
+xctool -workspace "$TESTS_PROJECT_ROOT/$TESTS_PROJECT_NAME.xcworkspace" -scheme $TESTS_PROJECT_NAME clean build test
