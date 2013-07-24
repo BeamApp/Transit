efd6db7830669ed0dcfee82b89841a73f4007f60
Marcel Jackwerth <marceljackwerth@gmail.com>
2013-07-24 12:55:11 +0200

    [Android] Trutyh/Falsy-Helpers to replicate JS-behavior

--- source/java-generic/src/com/getbeamapp/transit/common/TransitObject.java
+++ source/java-generic/src/com/getbeamapp/transit/common/TransitObject.java
@@ -12,5 +12,13 @@ public abstract class TransitObject {
     public Object callMember(String key, Object... arguments) {
         return getContext().eval("@[@].apply(@, @)", this, key, this, arguments);
     }
+
+    public static boolean isTruthy(Object o) {
+        return !isFalsy(o);
+    }
+
+    public static boolean isFalsy(Object o) {
+        return o == null || o.equals(false) || o.equals(0) || o.equals(Float.NaN) || o.equals("");
+    }
     
 }
